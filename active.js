5354094f3630d8614d3d2651ee7f5590311c1acf
tomcat-echo <2332046984@qq.com>
2022-05-10 18:45:38 +0800

    书海网

---
+++ active.js
@@ -0,0 +1,8 @@
+hide = function(li){
+	var a = li.getElementsByTagName("ul")[0];
+	a.style.display = "none";
+}
+show = function(li){
+	var a = li.getElementsByTagName("ul")[0];
+	a.style.display = "block";
+}
